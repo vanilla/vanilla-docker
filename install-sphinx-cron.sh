68863d8b0bfd989e4242d301bfc95aabd0992f84
alexander.kim <kim.alexander.ca@gmail.com>
2019-02-27 11:49:32 -0500

    add check if cron is already installed

--- install-sphinx-cron.sh
+++ install-sphinx-cron.sh
@@ -1,5 +1,10 @@
 #!/usr/bin/env bash
 
-(crontab -l ; echo "* * * * * /usr/local/bin/docker exec -t sphinx bash /root/index.delta.sh") | crontab -
+installed=$(crontab -l | grep index.delta.sh | wc -l)
 
-(crontab -l ; echo "*/5 * * * * /usr/local/bin/docker exec -t sphinx bash /root/index.all.sh") | crontab -
+if [ $installed -gt 0 ]; then
+    echo -e "Sphinx cron jobs seem already installed!\nCheck: crontab -l"
+else
+    (crontab -l ; echo "* * * * * /usr/local/bin/docker exec -t sphinx bash /root/index.delta.sh") | crontab -
+    (crontab -l ; echo "*/5 * * * * /usr/local/bin/docker exec -t sphinx bash /root/index.all.sh") | crontab -
+fi
