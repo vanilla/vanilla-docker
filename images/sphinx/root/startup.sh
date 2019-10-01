5975369fbb2789aa706152cb677c99bd04517ec5
Chris Chabilall <chris.c@vanillaforums.com>
2019-10-01 14:19:16 -0400

    Modify vanilla-docker to allow sphinx test locally

--- images/sphinx/root/startup.sh
+++ images/sphinx/root/startup.sh
@@ -1,4 +1,9 @@
+
 #!/bin/bash
+
+sed -i 's/{MYSQL_HOST}/'"$MYSQL_HOST"'/g' /usr/local/etc/sphinx/conf.d/sphinx.conf
+sed -i 's/{MYSQL_USER}/'"$MYSQL_USER"'/g' /usr/local/etc/sphinx/conf.d/sphinx.conf
+sed -i 's/{MYSQL_DATABASE}/'"$MYSQL_DATABASE"'/g' /usr/local/etc/sphinx/conf.d/sphinx.conf
 
 until mysql -h"database" -u"root" -e"quit"; do
   sleep 1
@@ -6,5 +11,14 @@ done
 
 >&2 echo "MySQL is now available."
 
+#until mysql -h"127.0.0.1" -u"circleci" -e"use vanilla_test; SELECT 1 FROM GDN_Discussion LIMIT 1;"; do
+#  sleep 1
+#done
+#
+#>&2 echo "GDN_Discussion table created."
+
+socat tcp-l:9399,fork system:/root/listen.9399.sh &
+
 service crond start && indexer --all && searchd --nodetach
+
 
